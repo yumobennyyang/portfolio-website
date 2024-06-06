fc64e58ff9007736b4deeb1798e76936c7658061
yumobennyyang <bennyyang2030@gmail.com>
2024-06-06 16:33:09 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -490,8 +490,8 @@ export default function PortfolioIndex() {
       <div style={{ background: "#111111" }} className=" darkBackground pointer-events-none  -z-40 w-full h-full fixed" />
 
 
-      <div className="gradient gradient-background z-20 w-screen   pointer-events-none  fixed" />
-      <div className="gradient gradient-blur z-20 w-screen   pointer-events-none  fixed">
+      <div className="gradient gradient-background z-20 w-screen hidden  pointer-events-none  fixed" />
+      <div className="gradient gradient-blur z-20 w-screen hidden  pointer-events-none  fixed">
         <div></div>
         <div></div>
         <div></div>
