245e10056883c09535966a629425e829d3e11a83
yumobennyyang <bennyyang2030@gmail.com>
2024-06-17 17:49:14 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -225,7 +225,7 @@ export default function PortfolioIndex() {
             scaleX: changeX,
             scaleY: changeY,
             //top: 'calc(-' + (elementHeight - 56) / 2 + 'px - 56px - 24px)',
-            top: 'calc(-' + (elementHeight - 56) / 2 + 'px - 56px - 24px - 50%)',
+            top: 'calc(-' + (elementHeight - 56) / 2 + 'px - 56px - 24px + 50%)',
             filter: "blur(0px)",
             scrollTrigger: {
               trigger: element,
