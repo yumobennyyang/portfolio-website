0f4434866d29718aa8226a7cb23f94be618652c7
yumobennyyang <bennyyang2030@gmail.com>
2024-11-05 14:23:22 -0500

    update

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,12 +2,9 @@ import React, { useState, useEffect, useRef } from 'react';
 import { projects } from '../data/projects';
 import styles from './Modal.module.css';
 import Image from 'next/image';
+import localFont from 'next/font/local';
 
-import localFont from "next/font/local";
-
-//const regularText = localFont({ src: '../fonts/PPNeueMontreal-Book.otf' });
 const regularText = localFont({ src: '../fonts/SF-Pro/SF-Pro-Text-Regular.otf' });
-//const text = localFont({ src: '../fonts/PPNeueMontreal-Medium.otf' });
 const text = localFont({ src: '../fonts/SF-Pro/SF-Pro-Display-Medium.otf' });
 const description = localFont({ src: '../fonts/SF-Pro/SF-Pro-Text-Bold.otf' });
 
@@ -22,47 +19,32 @@ const ProjectList = ({ onSelect, selectedProjectId }) => {
     if (selectedProjectId && selectedElementRef.current) {
       const elem = selectedElementRef.current;
 
-      if (typeof window !== "undefined") {
-
-        const minWidth = window.matchMedia("(min-width: 768px)").matches
+      // Ensure window is defined (client-side execution)
+      if (typeof window !== 'undefined') {
+        const minWidth = window.matchMedia('(min-width: 768px)').matches
           ? Math.min(window.innerWidth - 56, 960)
           : window.innerWidth - 24;
 
         const scaleX = minWidth / elem.offsetWidth;
         const scaleY = (window.innerHeight - 20) / elem.offsetHeight + 1; // 20 is the top margin
-
         setScaleValues({ scaleX, scaleY });
 
         const rect = elem.getBoundingClientRect();
-        const offsetTop = rect.top;
-        const translateY = 20 - offsetTop; //20 is the top margin
-
-        const offsetLeft = rect.left;
-        const elemCenterX = offsetLeft + rect.width / 2;
-        const screenCenterX = window.innerWidth / 2;
-        const translateX = screenCenterX - elemCenterX;
-
+        const translateY = 20 - rect.top; // 20 is the top margin
+        const translateX = window.innerWidth / 2 - (rect.left + rect.width / 2);
         setTranslateValues({ translateX, translateY });
 
-
-
-
-        // Calculate itemScale with the conditional adjustment
-        const itemScale = minWidth / (elem.offsetHeight - 84) / 3 * 2;
-
-
+        const itemScale = (minWidth / (elem.offsetHeight - 84)) * (2 / 3);
         setItemScale({ itemScale });
 
-        // Delay hiding the element by 0.5 seconds
         setTimeout(() => {
           setIsHidden(true);
         }, 500);
       }
     } else {
-      // Reset the hidden state when deselecting
       setIsHidden(false);
     }
-  }, [selectedProjectId]); // Trigger recalculation on selectedProjectId change
+  }, [selectedProjectId]);
 
   const handleClick = (id) => {
     onSelect(id);
@@ -74,55 +56,46 @@ const ProjectList = ({ onSelect, selectedProjectId }) => {
         <div
           key={project.id}
           ref={selectedProjectId === project.id ? selectedElementRef : null}
-          className={`project  pb-4 ${styles.projectItem} ${selectedProjectId === project.id ? `${styles.scaled} ${styles.layerShadowDark}` : ''} ${isHidden && selectedProjectId === project.id ? styles.hidden : ''}`}
+          className={`project pb-4 ${styles.projectItem} ${selectedProjectId === project.id ? `${styles.scaled} ${styles.layerShadowDark}` : ''} ${isHidden && selectedProjectId === project.id ? styles.hidden : ''}`}
           onClick={() => handleClick(project.id)}
           style={{ breakInside: 'avoid' }}
-
         >
-          <div className=" w-full text-zinc-950 tracking-wide  rounded-xl  ">
+          <div className="w-full text-zinc-950 tracking-wide rounded-xl">
             <div
-              className={`${styles.projectItem} w-full flex-col relative flex group bg-white rounded-xl layer-shadow layer-shadow-hover sm:hover:scale-[1.01] sm:hover:scale-z-[1.01] duration-300 ease-[cubic-bezier(0,0,.5,1)]`}
-              style={selectedProjectId === project.id ? { pointerEvents: 'none', borderRadius: `${6}px`, transform: ` translate(${translateValues.translateX}px, ${translateValues.translateY}px) scale(${scaleValues.scaleX}, ${scaleValues.scaleY}) ` } : {}}
+              className={`${styles.projectItem} w-full flex-col relative flex group bg-white rounded-xl layer-shadow layer-shadow-hover sm:hover:scale-[1.01] duration-300 ease-[cubic-bezier(0,0,.5,1)]`}
+              style={
+                selectedProjectId === project.id
+                  ? {
+                      pointerEvents: 'none',
+                      borderRadius: `${6}px`,
+                      transform: `translate(${translateValues.translateX}px, ${translateValues.translateY}px) scale(${scaleValues.scaleX}, ${scaleValues.scaleY})`
+                    }
+                  : {}
+              }
             >
-
               {project.image && (
-                <div className="  overflow-hidden rounded-xl  duration-300  origin-top "
-                  style={selectedProjectId === project.id
-                    ? { transform: `scale(${(1 / scaleValues.scaleX) * itemScale.itemScale}, ${(1 / scaleValues.scaleY) * itemScale.itemScale}) ` }
-                    : {}}>
-                  <img className="object-cover !border-none !rounded-none" src={project.image.src} alt={project.title} width={project.image.width} height={project.image.height} />
+                <div
+                  className="overflow-hidden rounded-xl duration-300 origin-top"
+                  style={
+                    selectedProjectId === project.id
+                      ? {
+                          transform: `scale(${(1 / scaleValues.scaleX) * itemScale.itemScale}, ${(1 / scaleValues.scaleY) * itemScale.itemScale})`
+                        }
+                      : {}
+                  }
+                >
+                  <Image className="object-cover !border-none !rounded-none" src={project.image.src} alt={project.title} width={project.image.width} height={project.image.height} />
                 </div>
               )}
-              {project.video && (
-                <div className="overflow-hidden rounded-xl  duration-300  origin-top"
-                  style={selectedProjectId === project.id
-                    ? { transform: `scale(${(1 / scaleValues.scaleX) * itemScale.itemScale}, ${(1 / scaleValues.scaleY) * itemScale.itemScale}) ` }
-                    : {}}>
-
-                  <video
-                    playsInline
-                    autoPlay
-                    muted
-                    loop
-                    className="object-cover"
-                    width={project.video.width}
-                    height={project.video.height}
-                  >
-                    <source src={project.video.src} type="video/mp4" />
-                  </video>
-                </div>
-              )}
-
             </div>
-            <div style={selectedProjectId === project.id
-              ? { opacity: '0', transition: 'opacity 0.1s' }
-              : {}}
-              className="z-20 px-1 pb-2 relative w-full flex-row">
-              <div className={` w-full pt-[10px] pb-[1px] truncate text-2xl tracking-[.007em] ${text.className}`}>{project.title}</div>
-              <div className={` text-zinc-500 opacity-80 w-full truncate  text-xs uppercase tracking-wider ${regularText.className}`}>{project.description}</div>
+            <div
+              style={selectedProjectId === project.id ? { opacity: '0', transition: 'opacity 0.1s' } : {}}
+              className="z-20 px-1 pb-2 relative w-full flex-row"
+            >
+              <div className={`w-full pt-[10px] pb-[1px] truncate text-2xl tracking-[.007em] ${text.className}`}>{project.title}</div>
+              <div className={`text-zinc-500 opacity-80 w-full truncate text-xs uppercase tracking-wider ${regularText.className}`}>{project.description}</div>
             </div>
           </div>
-
         </div>
       ))}
     </div>
