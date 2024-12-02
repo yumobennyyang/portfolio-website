916f2aad724a85787d0dc3e659135e4b8ead2704
yumobennyyang <bennyyang2030@gmail.com>
2024-12-02 06:43:58 -0500

    update

--- src/data/tencent.js
+++ src/data/tencent.js
@@ -6,52 +6,29 @@ const tencent = {
       width: 1024,
       height: 1024,
     },
-    overview: "During the summer of 2023 I interned at Tencent’s Interactive Entertainment Group (IEG) as an interface and interaction designer. The majority of my work involved creating HTML5 visuals and fine-tuning UI/UX for the Tencent Games Joy Club mobile app, which I am unfortunately unable to share. However, I was also assigned a separate solo task of a complete overhaul of the B2E recommender system used to promote content across all Tencent gaming apps. On this page I will go through snippets of my redesign of the Dora Platform.",
+    overview: "During the summer of 2023 I interned at Tencent’s Interactive Entertainment Group (IEG) as an interface and interaction designer. I was assigned the solo task of a complete overhaul of the B2E recommender system used to promote content across all Tencent gaming apps. On this page are snippets of my redesign of the Dora Platform, a formerly-in-bad-shape web app actively used by 500+ employees.",
     tools: ["Photoshop", "Figma", "CSS", "Midjourney"],
     duration: "2 months",
     description: "Interaction Design Internship",
     content: [
       { "type": "line", "text": "" },
-      { "type": "section", "text": "Old UI" },
+
   
+      { "type": "image", "src": "images/tencent/tencent1.png" },
       { "type": "image", "src": "images/tencent/tencent2.png" },
+      { "type": "image", "src": "images/tencent/tencent3.png" },
+      { "type": "image", "src": "images/tencent/tencent4.png" },
+      { "type": "image", "src": "images/tencent/tencent5.png" },
+      { "type": "image", "src": "images/tencent/tencent6.png" },
+      { "type": "image", "src": "images/tencent/tencent7.png" },
+      { "type": "image", "src": "images/tencent/tencent8.png" },
+      { "type": "image", "src": "images/tencent/tencent9.png" },
+      { "type": "image", "src": "images/tencent/tencent10.png" },
+      { "type": "image", "src": "images/tencent/tencent11.png" },
+      { "type": "image", "src": "images/tencent/tencent12.png" },
+      { "type": "image", "src": "images/tencent/tencent13.png" },
 
-      { "type": "line", "text": "" },
-      { "type": "section", "text": "The Problem" },
-      { "type": "text", "text": "The UI is very outdated, with almost no consideration for reponsiveness, scattering out the layout and creating overflows when the window is even slightly adjusted. The UX went through many bandaid solutions, resulting in stray buttons and links that serve the same purpose or point to the same pages, making navigating through the forms very confusing. The design under each section is also quite different as each section was worked on by different engineers at different times without a designer overseeing the conformity." },
-      { "type": "text", "text": "I was quite surprised to see the site in such bad shape since hundreds of employees are actively using it on a daily basis. The product manager of this site gave me complete creative freedom in the design. Though, as requested by my team leader, I am to base the color palette off of the larger entreprise that the platform serves under, Tencent Games Joy Club, for brand consistency." },
-  
-      { "type": "line", "text": "" },
-      { "type": "section", "text": "UX Restructuring" },
-      { "type": "image", "src": "/images/tencent/tencent3.png" },
-  
-      { "type": "line", "text": "" },
-      { "type": "section", "text": "Style Guide" },
-      { "type": "image", "src": "/images/tencent/tencent4.png" },
-      { "type": "image", "src": "/images/tencent/tencent5.png" },
-      { "type": "image", "src": "/images/tencent/tencent6.png" },
-      { "type": "image", "src": "/images/tencent/tencent7.png" },
-  
-      { "type": "line", "text": "" },
-      { "type": "section", "text": "Responsive Layout Grid" },
-      { "type": "image", "src": "/images/tencent/tencent8.png" },
 
-      { "type": "line", "text": "" },
-      { "type": "section", "text": "Home" },
-      { "type": "image", "src": "/images/tencent/tencent9.1.png" },
-      { "type": "image", "src": "/images/tencent/tencent9.2.png" },
-
-      { "type": "line", "text": "" },
-      { "type": "section", "text": "Table" },
-      { "type": "image", "src": "/images/tencent/tencent10.png" },
-
-      { "type": "line", "text": "" },
-      { "type": "section", "text": "Form" },
-      { "type": "image", "src": "/images/tencent/tencent11.png" },
-
-      { "type": "line", "text": "" },
-      { "type": "section", "text": "Navigation" },
-      { "type": "image", "src": "/images/tencent/tencent12.png" },
 
 
     ],
