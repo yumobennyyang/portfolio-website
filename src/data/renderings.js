a60c7d5bd14f9b92a8a7388455d3ed4b35371684
yumobennyyang <bennyyang2030@gmail.com>
2024-12-11 03:00:33 -0500

    update

--- src/data/renderings.js
+++ src/data/renderings.js
@@ -9,23 +9,42 @@ const renderings = {
   overview: "Static images created with various tools.",
   description: "Digitals and Forms as Pixels",
   content: [
+
+
     { "type": "line", "text": "" },
+    { "type": "section", "text": "Green Machine, photoshop, 2021." },
+    { "type": "text", "text": "A gift to Deerfield Academy." },
+    { "type": "image", "src": "/images/renderings/render1.png" },
 
-    { "type": "image", "src": "/images/renderings/renderings1.png" },
-    { "type": "text", "text": "Green Machine, photoshop, 2021. A gift to Deerfield Academy" },
+    { "type": "line", "text": "" },
+    { "type": "section", "text": "Fake Fish, blender, 2022." },
+    { "type": "text", "text": "An Homage to Jumping Trout (1889) by Winslow Homer." },
+    { "type": "image", "src": "/images/renderings/render2.png" },
 
-    { "type": "image", "src": "/images/renderings/renderings2.png" },
-    { "type": "text", "text": "Sky Behind the Fence, photoshop, 2022. Part of a larger photography series about fences" },
 
-    { "type": "image", "src": "/images/renderings/renderings3.png" },
-    { "type": "text", "text": "Plastic Fish, blender, 2022. An Homage to Jumping Trout (1889) by Winslow Homer." },
+    { "type": "line", "text": "" },
+    { "type": "section", "text": "Sky Behind the Fence, photoshop, 2022." },
+    { "type": "text", "text": "Part of a larger photography series about fences" },
+    { "type": "image", "src": "/images/renderings/render3.png" },
 
-    { "type": "image", "src": "/images/renderings/renderings4.png" },
-    { "type": "text", "text": "Donut, dalle2 + photoshop, 2022. Component of an album cover commission." },
 
-    { "type": "image", "src": "/images/renderings/renderings5.png" },
-    { "type": "image", "src": "/images/renderings/renderings6.png" },
-    { "type": "text", "text": "United States of Achromatism, photoshoop, 2020. I merged the faces of the 100 most powerful people in the US, 20 each from fields of religion, wealth, military, legislation, and education." },
+
+    { "type": "line", "text": "" },
+    { "type": "section", "text": "Donut, dalle2 + photoshop, 2022." },
+    { "type": "text", "text": "Experimenting with edge joining with AI." },
+    { "type": "image", "src": "/images/renderings/render4.png" },
+
+    { "type": "line", "text": "" },
+    { "type": "section", "text": "United States of Achromatism, photoshop, 2020." },
+    { "type": "text", "text": "I merged the faces of the 100 most powerful people in the US, 20 each from fields of religion, wealth, military, legislation, and education." },
+    { "type": "image", "src": "/images/renderings/render5.png" },
+
+
+    { "type": "line", "text": "" },
+    { "type": "section", "text": "The Cloud That Just Learned How To Fly, photoshop, 2024." },
+    { "type": "text", "text": "Prototype of a sculpture I plan on building out." },
+    { "type": "image", "src": "/images/renderings/render6.png" },
+
 
   ],
 
--- src/data/volumes.js
+++ src/data/volumes.js
@@ -9,20 +9,23 @@ const volumes = {
   overview: "The manipulation of a foam fead in the form of sculpture.",
   description: "Sculptures and Forms in Space",
   content: [
+
+
     { "type": "line", "text": "" },
+    { "type": "section", "text": "小丑的自塑像, found objects, 2024, 8x8x12" },
+    { "type": "image", "src": "/images/volumes/sculpt1.png" },
 
-    { "type": "image", "src": "/images/volumes/volume1.png" },
-    { "type": "text", "text": "小丑的自塑像, found objects, 2024, 8x8x12" },
+    { "type": "line", "text": "" },
+    { "type": "section", "text": "Euphemism for a Good Night's Sleep, ceramics + metalwork, 2023, 22x22x60" },
+    { "type": "image", "src": "/images/volumes/sculpt2.png" },
 
-    { "type": "image", "src": "/images/volumes/volume2.png" },
-    { "type": "text", "text": "Euphemism for a Good Night's Sleep, ceramics + metalwork, 2023, 22x22x60" },
+    { "type": "line", "text": "" },
+    { "type": "section", "text": "Watchdog in the Kitsch, found objects, 2020, 22x13x16" },
+    { "type": "image", "src": "/images/volumes/sculpt3.png" },
 
-    { "type": "image", "src": "/images/volumes/volume3.png" },
-    { "type": "text", "text": "Peril through the Lookingglass, found objects, 2021, 18x11x13" },
-
-    { "type": "image", "src": "/images/volumes/volume4.png" },
-    { "type": "text", "text": "Watchdog in the Kitsch, found objects, 2020, 22x13x16" },
-
+    { "type": "line", "text": "" },
+    { "type": "section", "text": "Peril through the Lookingglass, found objects, 2021, 18x11x13" },
+    { "type": "image", "src": "/images/volumes/sculpt4.png" },
   ],
 
 };
