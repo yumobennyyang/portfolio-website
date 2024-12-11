dda7a0bc252ef162b0edb3bda912a3e694230949
yumobennyyang <bennyyang2030@gmail.com>
2024-12-11 05:00:11 -0500

    update

--- src/data/playground.js
+++ src/data/playground.js
@@ -10,37 +10,52 @@ const playground = {
   description: "Interactive Experiments",
   tools: ["Figma", "Swift", "Three.js", "HTML/CSS/JS"],
   content: [
+
     { "type": "line", "text": "" },
+    { "type": "section", "text": "Gidd - Three.js" },
+    { "type": "text", "text": "An interactive blob with an inexplicable connection to your cursor." },
 
     { "type": "iframe", "src": "https://yumobennyyang.github.io/gidd-blob/" },
-    { "type": "text", "text": "Gidd is an interactive blob with an inexplicable connection to your cursor." },
+
+
+    { "type": "line", "text": "" },
+    { "type": "section", "text": "Fake Lighting - CSS" },
+    { "type": "text", "text": "Hover over the image for a fabricated 3D effect." },
 
     { "type": "iframe", "src": "https://hover-lighting.vercel.app/" },
-    { "type": "text", "text": "Hover over the image for a fake 3D effect." },
+
+
+    
+    { "type": "line", "text": "" },
+    { "type": "section", "text": "If responsive sites were designed like that hospital in the movie Idiocracy - Javascript" },
+    { "type": "text", "text": "Resize the window by dragging the bottom right corner." },
 
     {
       "type": "iframe",
       "src": "https://stgodsmemorialhospital.vercel.app/",
       "containerStyling": {
         "resize": "horizontal",
-        "width": "calc(100% / 0.75)",
-        "minWidth": "50%",
-        "maxWidth": "90%",
 
-        
+        "minWidth": "50%",
+        "maxWidth": "96%",
 
       },
       "iframeStyling": {
-        "width": "calc(120% / 0.75)",
-        "transform": "translate(-12.5%, -12.5%) scale(0.75)",
+
         "pointerEvents": "none",
-        "height": "500px",
+        "transform": "scale(1.04,1)",
+
       }
     },
-    { "type": "text", "text": "Drag the window to see what it would be like if responsive sites were designed like that hospital in the movie Idiocracy." },
+   
 
+
+        
+    { "type": "line", "text": "" },
+    { "type": "section", "text": "An experimental AI interface - Figma" },
+    { "type": "text", "text": "A creative content generator centered around an entity that self-chromatizes as it incrementally takes in instructions." },
     { "type": "video", "src": "/images/playground/playground3.mp4", autoplay: false, looping: false, },
-    { "type": "text", "text": "An experimental AI interface centered around an entity that self-chromatizes as it incrementally takes in instructions to create content." },
+   
 
   ],
 
