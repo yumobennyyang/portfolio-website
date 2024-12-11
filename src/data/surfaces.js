9a2cff93f1583a7bf7090094eaea225aba1ee2b9
yumobennyyang <bennyyang2030@gmail.com>
2024-12-11 03:00:33 -0500

    update

--- src/data/surfaces.js
+++ src/data/surfaces.js
@@ -10,36 +10,44 @@ const surfaces = {
     description: "Paintings and Forms on Paper",
     content: [
       { "type": "line", "text": "" },
-  
+      { "type": "section", "text": "Aquarium, oil on canvas, 2024, 12x20" },
       { "type": "image", "src": "/images/surfaces/paint1.png" },
-      { "type": "text", "text": "Aquarium, oil on canvas, 2024, 12x20" },
 
+      { "type": "line", "text": "" },
+      { "type": "section", "text": "Dagim, oil on panel, 2024, 12x12" },
       { "type": "image", "src": "/images/surfaces/paint2.png" },
-      { "type": "text", "text": "Dagim, oil on panel, 2024, 12x12" },
 
+      { "type": "line", "text": "" },
+      { "type": "section", "text": "Charlie is Back, oil on canvas, 2023, 24x36" },
       { "type": "image", "src": "/images/surfaces/paint3.png" },
-      { "type": "text", "text": "Charlie is Back, oil on canvas, 2023, 24x36" },
 
+      { "type": "line", "text": "" },
+      { "type": "section", "text": "The Forum, oil on canvas, 2022, 18x18" },
       { "type": "image", "src": "/images/surfaces/paint4.png" },
-      { "type": "text", "text": "The Forum, oil on canvas, 2022, 18x18" },
 
+      { "type": "line", "text": "" },
+      { "type": "section", "text": "老人, oil on canvas, 2022, 16x20" },
       { "type": "image", "src": "/images/surfaces/paint5.png" },
-      { "type": "text", "text": "老人, oil on canvas, 2022, 16x20" },
 
+      { "type": "line", "text": "" },
+      { "type": "section", "text": "Finale, pastel on linen, 2022, 24x36" },
       { "type": "image", "src": "/images/surfaces/paint6.png" },
-      { "type": "text", "text": "Finale, pastel on linen, 2022, 24x36" },
 
+      { "type": "line", "text": "" },
+      { "type": "section", "text": "Study of a Plant on a Sphere, woodcut print, 2022, 15x18" },
       { "type": "image", "src": "/images/surfaces/paint7.png" },
-      { "type": "text", "text": "Study of a Plant on a Sphere, woodcut print, 2022, 15x18" },
 
+      { "type": "line", "text": "" },
+      { "type": "section", "text": "Nights and Streets and Winds and Lights, charcoal, 2020, 14x17" },
       { "type": "image", "src": "/images/surfaces/paint8.png" },
-      { "type": "text", "text": "Nights and Streets and Winds and Lights, charcoal, 2020, 14x17" },
 
+      { "type": "line", "text": "" },
+      { "type": "section", "text": "Inhabitation I II III, oil on canvas, 2020, 10x20(x3)" },
       { "type": "image", "src": "/images/surfaces/paint9.png" },
-      { "type": "text", "text": "Inhabitation I II III, oil on canvas, 2020, 10x20(x3)" },
 
+      { "type": "line", "text": "" },
+      { "type": "section", "text": "Self Portrait, white charcoal, 2019, 19x25" },
       { "type": "image", "src": "/images/surfaces/paint10.png" },
-      { "type": "text", "text": "Self Portrait, white charcoal, 2019, 19x25" },
 
     ],
 
