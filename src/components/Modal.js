078ebce3e32642dcd167389c281c591b9764eb17
yumobennyyang <bennyyang2030@gmail.com>
2024-12-27 23:44:32 +0800

    update

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -27,10 +27,10 @@ const Modal = ({ isOpen, onClose, children, selectedProjectId }) => {
 
             timer = setTimeout(() => {
                 setShowModalContent(true);
-                //setMakeClickable(true);
+                setMakeClickable(true);
 
 
-            }, 500);// 0.5-second delay for modal content
+            }, 0);// 0.1-second delay for modal content
 
 
         } else {
@@ -41,7 +41,7 @@ const Modal = ({ isOpen, onClose, children, selectedProjectId }) => {
                 logoDiv.style.transition = '';
             }
             setShowModalContent(false);
-            //setMakeClickable(false);
+            setMakeClickable(false);
 
             document.body.style.overflow = '';
 
@@ -65,61 +65,61 @@ const Modal = ({ isOpen, onClose, children, selectedProjectId }) => {
 
     return (
         <div className={`${styles.modalOverlay} layerShadowDark fixed inset-0 w-screen h-screen flex items-end justify-center z-[10000] transition-all duration-300 ease-in-out`} onClick={onClose}>
-            {showModalContent && (
-                <div className={`${styles.modalContent} aboslute overflow-x-hidden md:max-h-[calc(100vh-20px)] max-h-[calc(100vh-48px)] top-12 md:top-5 bg-white rounded-t-2xl max-w-[960px] overflow-y-auto z-[10002]  mx-3 sm:mx-7 `} onClick={(e) => e.stopPropagation()}>
 
-                    
-                    <div className="hidden mx-auto gradient h-full gradient-background z-20 mt-5  w-full max-w-[960px]  pointer-events-none rounded-t-2xl fixed clipped" />
-                    <div className="hidden mx-auto gradient h-full  gradient-blur z-20 mt-5  w-full  max-w-[960px] pointer-events-none rounded-t-2xl fixed clipped">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-
-                    <button className={` fixed h-0 top-4 left-4 bg-transparent border-none cursor-pointer text-neutral-950 ${benny.className}`} onClick={onClose}>
+            <div className={`${styles.modalContent} ${styles.modalFadeIn} ${showModalContent ? styles.modalVisible : ''} aboslute overflow-x-hidden md:max-h-[calc(100vh-20px)] max-h-[calc(100vh-48px)] top-12 md:top-5 bg-white rounded-t-2xl max-w-[960px] overflow-y-auto z-[10002]  mx-3 sm:mx-7 ${makeClickable ? '' : 'pointer-events-none'}`} onClick={(e) => e.stopPropagation()}>
 
 
-
-
-
-                        <div className={`flex mix-blend-difference sm:mx-4  my-2 z-50 left-0 top-0 fixed w-auto leading-6  text-sm `}>
-
-
-                            <p className=" justify-center pl-4 pr-1 py-2  w-auto">
-
-                                <span>close</span>
-                                {/* <span>↗</span>*/}
-                            </p>
-
-
-
-                        </div>
-                        <div className={`flex mix-blend-difference sm:mx-4  my-2 z-50 left-0 top-0 fixed w-auto leading-6  text-sm `}>
-
-
-                            <p className=" justify-center pl-4 pr-1 py-2  w-auto underline underline-offset-4 decoration-[0.2px] hover:no-underline group">
-
-                                <span>close</span>
-                                {/* <span>↗</span>*/}
-                            </p>
-
-
-
-                        </div>
-                    </button>
-
-
-
-                    <div className="z-[999999999999] overflow-y-auto mb-16 ">
-
-
-                        {children}
-                    </div>
+                <div className="hidden mx-auto gradient h-full gradient-background z-20 mt-5  w-full max-w-[960px]  pointer-events-none rounded-t-2xl fixed clipped" />
+                <div className="hidden mx-auto gradient h-full  gradient-blur z-20 mt-5  w-full  max-w-[960px] pointer-events-none rounded-t-2xl fixed clipped">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </div>
-            )}
+
+                <button className={` fixed h-0 top-4 left-4 bg-transparent border-none cursor-pointer text-neutral-950 ${benny.className}`} onClick={onClose}>
+
+
+
+
+
+                    <div className={`flex mix-blend-difference sm:mx-4  my-2 z-50 left-0 top-0 fixed w-auto leading-6  text-sm `}>
+
+
+                        <p className=" justify-center pl-4 pr-1 py-2  w-auto">
+
+                            <span>close</span>
+                            {/* <span>↗</span>*/}
+                        </p>
+
+
+
+                    </div>
+                    <div className={`flex mix-blend-difference sm:mx-4  my-2 z-50 left-0 top-0 fixed w-auto leading-6  text-sm `}>
+
+
+                        <p className=" justify-center pl-4 pr-1 py-2  w-auto underline underline-offset-4 decoration-[0.2px] hover:no-underline group">
+
+                            <span>close</span>
+                            {/* <span>↗</span>*/}
+                        </p>
+
+
+
+                    </div>
+                </button>
+
+
+
+                <div className="z-[999999999999] overflow-y-auto mb-16 ">
+
+
+                    {children}
+                </div>
+            </div>
+
         </div>
     );
 };
