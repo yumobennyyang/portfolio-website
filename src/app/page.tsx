e2bfd13ba6d9a4a2424d43fc6ee0bcfc99e7cea3
yumobennyyang <bennyyang2030@gmail.com>
2025-02-20 16:31:24 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -653,7 +653,7 @@ export default function PortfolioIndex() {
 
 
         <div
-          className={`  mt-[104px] hideText left-1/2 -translate-x-1/2 relative leading-6 justify-between  text-zinc-50 text-sm ${benny.className}        `}
+          className={`  mt-[104px] hideText origin-top left-1/2 -translate-x-1/2 relative leading-6 justify-between  text-zinc-50 text-sm ${benny.className}        `}
         >
 
           <div id="mainText" className="mainText justify-center mx-auto my-6  p-0.5">
