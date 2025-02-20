53f6a5e2110125ce9bb22e0c146712b809366c12
yumobennyyang <bennyyang2030@gmail.com>
2025-02-20 16:45:32 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -227,7 +227,7 @@ export default function PortfolioIndex() {
             scaleX: changeX,
             scaleY: changeY,
 
-            top: `calc(-${(elementHeight - 56) / 2}px - 56px - 24px - 56px)`,
+            top: `calc(-${(elementHeight - 56) / 2}px - 56px - 24px - 56px - 24px)`,
             filter: "blur(0px)",
             scrollTrigger: {
               trigger: element,
@@ -656,7 +656,7 @@ export default function PortfolioIndex() {
           className={`  mt-[104px] hideText left-1/2 -translate-x-1/2 relative leading-6 justify-between  text-zinc-50 text-sm ${benny.className}        `}
         >
 
-          <div id="mainText" className="mainText justify-center mx-auto my-6  p-0.5 origin-top">
+          <div id="mainText" className="mainText justify-center mx-auto my-6  p-0.5 ">
             <span id="intro" className="split-type ">Benny is an artist and designer currently studying computer science and visual arts at Columbia University.</span>
             <span className="flashing disappear">_</span>
             <br></br><br></br>
