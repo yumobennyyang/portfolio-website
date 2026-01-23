6f8de28eb68c05851fa35835fa442719298a0cac
yumobennyyang <123268757+yumobennyyang@users.noreply.github.com>
2026-01-23 03:16:43 -0500

    update

--- src/data/caiStudio.js
+++ src/data/caiStudio.js
@@ -22,8 +22,27 @@ const caiStudio = {
   content: [
 
 
+  
+    { "type": "section", "text": "Sequence Visuaization" },
+    { "type": "image", "src": "/images/cai-studio/echoanta.png", "secondUrl":"/images/cai-studio/EchoAnta-p5.mov", "videoInlineStyles": { position: 'absolute', top: '7.3%', left: '66.8%', width: '32.9%' }},
 
-    // { "type": "section", "text": "Final Scenes" },
+    { "type": "image", "src": "/images/cai-studio/synthview.png", "secondUrl":"/images/cai-studio/SynthView-p5.mov", "videoInlineStyles": { position: 'absolute', top: '7.3%', left: '66.8%', width: '32.9%' } },
+    { "type": "image", "src": "/images/cai-studio/altcog.png", "secondUrl":"/images/cai-studio/AltCog-p5.mov", "videoInlineStyles": { position: 'absolute', top: '7.3%', left: '66.8%', width: '32.9%' } },
+    { "type": "image", "src": "/images/cai-studio/logicloom.png", "secondUrl":"/images/cai-studio/LogicLoom-p5.mov", "videoInlineStyles": { position: 'absolute', top: '7.3%', left: '66.8%', width: '32.9%' } },
+    { "type": "image", "src": "/images/cai-studio/humavisor.png", "secondUrl":"/images/cai-studio/HumaVisor-p5.mov", "videoInlineStyles": { position: 'absolute', top: '7.3%', left: '66.8%', width: '32.9%' } },
+{ "type": "image", "src": "/images/cai-studio/snake.png", "secondUrl":"/images/cai-studio/tanchishe-p5.mov", "videoInlineStyles": { position: 'absolute', top: '62.7%', left: '66.8%', width: '32.9%' } },
+
+    { "type": "section", "text": "Viewpoint Rendering" },
+    { "type": "image", "src": "/images/cai-studio/echoanta2.png", "secondUrl":"/images/cai-studio/echoanta.mov", "videoInlineStyles": { position: 'absolute', top: '7.3%', left: '66.8%', width: '32.9%' }},
+    { "type": "image", "src": "/images/cai-studio/synthview2.png", "secondUrl":"/images/cai-studio/synthview.mov", "videoInlineStyles": { position: 'absolute', top: '7.3%', left: '66.8%', width: '32.9%' } },
+    { "type": "image", "src": "/images/cai-studio/altcog2.png", "secondUrl":"/images/cai-studio/altcog.mov", "videoInlineStyles": { position: 'absolute', top: '7.3%', left: '66.8%', width: '32.9%' } },
+    { "type": "image", "src": "/images/cai-studio/logicloom2.png", "secondUrl":"/images/cai-studio/logicloom.mov", "videoInlineStyles": { position: 'absolute', top: '7.3%', left: '66.8%', width: '32.9%' } },
+    { "type": "image", "src": "/images/cai-studio/humavisor2.png", "secondUrl":"/images/cai-studio/humavisor.mov", "videoInlineStyles": { position: 'absolute', top: '7.3%', left: '66.8%', width: '32.9%' } },
+
+
+
+
+    { "type": "section", "text": "Final Scenes" },
         { "type": "section", "text": "" },
     { "type": "video", "src": "/images/cai-studio/EchoAnta-final.mp4" , autoplay: true, looping: true},
     { "type": "video", "src": "/images/cai-studio/SynthView-final.mp4" , autoplay: true, looping: true},
@@ -33,12 +52,6 @@ const caiStudio = {
     { "type": "video", "src": "/images/cai-studio/tanchishe-final.mp4", autoplay: true, looping: true },
 
 
-  
-    { "type": "section", "text": "Sequence Visuaization" },
-    { "type": "image", "src": "/images/cai-studio/cai1.png" },
-
-    { "type": "section", "text": "Viewpoint Rendering" },
-    { "type": "image", "src": "/images/cai-studio/cai2.png" },
 
     // { "type": "line", "text": "" },
     // { "type": "section", "text": "Miscellaneous - Blender, AutoCAD" },
