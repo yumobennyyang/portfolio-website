7275471eea43b163934059bbe2794aab8bd677d2
yumobennyyang <123268757+yumobennyyang@users.noreply.github.com>
2026-01-23 03:47:15 -0500

    update

--- src/data/spatialUX.js
+++ src/data/spatialUX.js
@@ -14,10 +14,16 @@ const spatialUX = {
   team: ["Yuhang Qiu", "Zhengrui Hao"],
   content: [
 
-    { "type": "section", "text": "" },
-
+    { "type": "section", "text": "[1] Brick by brick" },
     { "type": "image", "src": "/images/spatial-ux/spatial1.png" },
+    { "type": "text", "text": "more to come." },
 
+    { "type": "section", "text": "[2] Interform" },
+    { "type": "image", "src": "/images/spatial-ux/spatial2.png" },
+    { "type": "text", "text": "more to come." },
+
+    { "type": "section", "text": "[3] See-saw" },
+    { "type": "image", "src": "/images/spatial-ux/spatial3.png" },
     { "type": "text", "text": "more to come." },
 
 
