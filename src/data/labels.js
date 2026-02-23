cde29827436d2dc31e8014f72687821b3136dd80
yumobennyyang <123268757+yumobennyyang@users.noreply.github.com>
2026-02-23 17:50:55 -0500

    update

--- src/data/labels.js
+++ src/data/labels.js
@@ -20,19 +20,27 @@ const label3 = {
 
     
         { "type": "section", "text": "Echo", "secondaryText": "Ripple Effect" },
+        { "type": "video", "src": "/images/labels/real_label1.mp4", autoplay: true, looping: true },
+
+
         { "type": "video", "src": "/images/labels/label1.mov", autoplay: true, looping: true, videoStyles: "contrast-[105%]" },
 
 
         { "type": "section", "text": "Liquid Glass", "secondaryText": "The shader distorts the background using physics-based refraction and chromatic aberration. During motion, the shader rotates and scales the coordinate space based on velocity, stretching the object along its path while compressing it perpendicularly to preserve its perceived volume." },
-        { "type": "video", "src": "/images/labels/label3.mov", autoplay: true, looping: true, videoStyles: "contrast-[105%]" },
 
+    { "type": "video", "src": "/images/labels/real_label3.mp4", autoplay: true, looping: true },
+        { "type": "video", "src": "/images/labels/label3.mov", autoplay: true, looping: true, videoStyles: "contrast-[105%]" },
     { "type": "image", "src": "/images/labels/label3.png" },
 
 
             { "type": "section", "text": "Elastic Deformation", "secondaryText": "Expansion and compression calculated from Poisson's ratio. On stretch, the material appears to thin out, revealing a red, dotted inner lining, while the haptics deliver subtle, crisp thuds in sync." },
-        { "type": "video", "src": "/images/labels/label2.mov", autoplay: true, looping: true, videoStyles: "contrast-[105%]" },
+        { "type": "video", "src": "/images/labels/real_label2.mp4", autoplay: true, looping: true },
 
     { "type": "image", "src": "/images/labels/label2.png" },
+
+
+        { "type": "section", "text": "Smooth Union", "secondaryText": "SDF union in Metal. Feeding the dynamic island makes it fatter." },
+        { "type": "video", "src": "/images/labels/real_label4.mp4", autoplay: true, looping: true },
 
 
 
